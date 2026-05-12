08d35972ce1aa683b1e71ae360fdc62c16e8339e
Godfrey Maina <bbt5.11698.24@student.tharaka.ac.ke>
2026-05-12 11:45:09 +0300

    Update input prompt for guessing game
    test your luck

--- Guesing game.py
+++ Guesing game.py
@@ -4,7 +4,7 @@ guess_count = 0
 guess_limit = 3
 # The user is prompted to enter their guess, and the program checks if the guess is correct. If the user guesses the number correctly, they win. If they run out of attempts, they lose. 
 while guess_count < guess_limit:
-    guess = int(input("Guess: "))
+    guess = int(input("Guess your lucky number: "))
     guess_count +=  1
     if guess == secret_number:
         print("You won!")
